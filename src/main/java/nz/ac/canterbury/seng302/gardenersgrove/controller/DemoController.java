811c88dc9c919a39d5d89b32243c3163d6311656
Hanan Fokkens <hfo22@uclive.ac.nz>
2024-02-27 15:53:27 +1300

    Add simple home page with link to create garden page
    With Ahmick and Stephen

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/DemoController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/DemoController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * This is a basic spring boot controller, note the @link{Controller} annotation which defines this.
+ * This is a basic spring boot controller, note the @link{Controller} annotation
+ * which defines this.
  * This controller defines endpoints as functions with specific HTTP mappings
  */
 @Controller
@@ -16,23 +17,17 @@ public class DemoController {
     Logger logger = LoggerFactory.getLogger(DemoController.class);
 
     /**
-     * Redirects GET default url '/' to '/demo'
-     * @return redirect to /demo
-     */
-    @GetMapping("/")
-    public String home() {
-        logger.info("GET /");
-        return "redirect:./demo";
-    }
-
-    /**
-     * Gets the thymeleaf page representing the /demo page (a basic welcome screen with some links)
-     * @param name url query parameter of user's name
-     * @param model (map-like) representation of data to be used in thymeleaf display
+     * Gets the thymeleaf page representing the /demo page (a basic welcome screen
+     * with some links)
+     * 
+     * @param name  url query parameter of user's name
+     * @param model (map-like) representation of data to be used in thymeleaf
+     *              display
      * @return thymeleaf demoTemplate
      */
     @GetMapping("/demo")
-    public String getTemplate(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+    public String getTemplate(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Model model) {
         logger.info("GET /demo");
         model.addAttribute("name", name);
         return "demoTemplate";
---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/GardenController.java
@@ -0,0 +1,22 @@
+package nz.ac.canterbury.seng302.gardenersgrove.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * This is the controller for the home page.
+ */
+@Controller
+public class GardenController {
+    Logger logger = LoggerFactory.getLogger(GardenController.class);
+
+    /**
+     * Renders the template.
+     */
+    @GetMapping("/gardens/create")
+    public String gardenCreate() {
+        return "createGarden";
+    }
+}
