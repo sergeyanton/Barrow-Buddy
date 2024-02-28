f96387fddd1e8746583b2db3b8f1f9ce361afa3d
Sergey Antonets <san177@uclive.ac.nz>
2024-02-28 22:47:44 +1300

    Began applying validation methods to user inputs

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/RegistrationController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/RegistrationController.java
@@ -1,15 +1,15 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
-import nz.ac.canterbury.seng302.gardenersgrove.entity.FormResult;
-import nz.ac.canterbury.seng302.gardenersgrove.service.FormService;
+import nz.ac.canterbury.seng302.gardenersgrove.dataCollection.RegistrationData;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import static nz.ac.canterbury.seng302.gardenersgrove.entity.User.*;
 
 @Controller
 public class RegistrationController {
@@ -29,8 +29,21 @@ public class RegistrationController {
     }
 
     @PostMapping("/register")
-    public String submitRegistration() {
+    public String submitRegistration(RegistrationData newUser) {
         logger.info("POST /register");
+        //dataCheck(newUser);
         return "redirect:./home";
     }
+
+    private String dataCheck(RegistrationData newUser){
+        Validator nameCheck = checkName(newUser.getfName());
+        if (!nameCheck.getStatus()){return nameCheck.getMessage();}
+        Validator surnameCheck = checkName(newUser.getlName());
+        if (!surnameCheck.getStatus()){return surnameCheck.getMessage();}
+        Validator emailCheck = checkEmail(newUser.getEmail());
+        if (!emailCheck.getStatus()){return emailCheck.getMessage();}
+
+        return "";
+    }
+
 }
