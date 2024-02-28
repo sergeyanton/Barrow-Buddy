6ff5e44373a5cb92e9401c2e4282ebf6447b718d
Zipporah Price <zpr12@uclive.ac.nz>
2024-02-28 15:46:52 +1300

    Created a base profile page to display the user information

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ProfileController.java
@@ -0,0 +1,22 @@
+package nz.ac.canterbury.seng302.gardenersgrove.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+
+    Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    /**
+     * Gets the thymeleaf page representing the /profile page (displaying user's information)
+     * @return thymeleaf profilePage
+     */
+    @GetMapping("/profile")
+    public String login() {
+        logger.info("GET /profile");
+        return "pages/profilePage";
+    }
+}
