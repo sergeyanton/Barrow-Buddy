5374d5626ee5a9fc69f4226354678a0078455870
Stephen Hockey (sho151) <sho151@uclive.ac.nz>
2024-02-29 23:20:06 +1300

    Deleted unused imports

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/HomeControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/HomeControllerTest.java
@@ -1,7 +1,5 @@
 package nz.ac.canterbury.seng302.gardenersgrove;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
