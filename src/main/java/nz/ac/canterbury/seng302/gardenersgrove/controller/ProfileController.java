d2a5eac7166a5b2c7ae742f05c43c7edc4790cb6
Sergey Antonets <san177@uclive.ac.nz>
2024-03-06 15:51:28 +1300

    Began writing the controller for edit profile page

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ProfileController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ProfileController.java
@@ -1,17 +1,26 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
+import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProfileController {
     Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping("/editProfile")
-    public String getHome() {
+    public String getEditProfilePage() {
         logger.info("GET /editProfile");
         return "pages/editProfilePage";
+    }
+    @PostMapping
+    public String editProfile(User currentuser, Model model) {
+        logger.info("POST /editProfile");
+        
+        return "redirect:/profile";
     }
 }
