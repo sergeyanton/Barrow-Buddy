29a5a7f02aa9b08cf264cffc7e4660ab07e533a0
Zipporah Price <zpr12@uclive.ac.nz>
2024-03-06 17:35:26 +1300

    Added encryption for storing user passwords in database.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/Validation/InputValidation.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/Validation/InputValidation.java
@@ -2,6 +2,7 @@ package nz.ac.canterbury.seng302.gardenersgrove.Validation;
 
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Validator;
 import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.time.LocalDate;
 
@@ -88,6 +89,26 @@ public class InputValidation {
             isValid.setValid(false,"Your password must be  at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.");
         }
         return isValid;
+    }
+
+    /**
+     * Hashes a given plain text password to be stored in the database
+     * @param password plain text String
+     * @return String of hashed password
+     */
+    public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+
+    /**
+     * Checks if a given plain text password is equal to a given hashed password.
+     * @param givenPassword plain text password
+     * @param hashedPassword to be compared with the givenPassword
+     * @return true if equal, otherwise false
+     */
+    public static boolean verifyPassword(String givenPassword, String hashedPassword) {
+        return BCrypt.checkpw(givenPassword, hashedPassword);
     }
 
 }
