a1b24442cd14c3ca33ac383793a8f3de55f38b76
Sergey Antonets <san177@uclive.ac.nz>
2024-03-06 17:22:05 +1300

    continue working on edit profile controller

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ProfileController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ProfileController.java
@@ -1,8 +1,11 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
 import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
+import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProfileController {
+
+    private final UserService userService;
+
     Logger logger = LoggerFactory.getLogger(ProfileController.class);
 
     @GetMapping("/editProfile")
@@ -20,7 +26,9 @@ public class ProfileController {
     @PostMapping
     public String editProfile(Model model) {
         logger.info("POST /editProfile");
-        
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        User u = userService.getUserByEmail(currentPrincipalName);
         return "redirect:/profile";
     }
 }
