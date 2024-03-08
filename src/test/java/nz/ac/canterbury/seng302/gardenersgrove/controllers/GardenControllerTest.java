dd5ca624bed78c3228a126b402ea4b95c13308ac
Hanan Fokkens <hfo22@uclive.ac.nz>
2024-03-08 13:47:24 +1300

    Add tests to ensure that create and edit garden endpoints are indeed validated

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/controllers/GardenControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/controllers/GardenControllerTest.java
@@ -1,95 +1,93 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import nz.ac.canterbury.seng302.gardenersgrove.controller.GardensController;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
-import nz.ac.canterbury.seng302.gardenersgrove.repository.GardenRepository;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = GardensController.class)
 @AutoConfigureMockMvc
 public class GardenControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
-    private GardenRepository gardenRepository;
-
-    @Autowired
+    @MockBean
     private GardenService gardenService;
 
-    @BeforeEach
-    public void setUp() {
-        gardenRepository.deleteAll();
-    }
-
     @Test
-    public void CreateGardenPost_WithValidGardenWithEmptySize_CreatesGardenWithNullSize()
-            throws Exception {
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/gardens/create").param("gardenName", "gardenName")
-                        .param("gardenLocation", "gardenLocation").param("gardenSize", ""))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.redirectedUrl("/gardens/1")).andDo(result -> {
-                    Garden createdGarden = gardenService.getGardenById(1);
-                    assertNull(createdGarden.getSize());
-                });
-    }
+    public void CreateGardenPost_WithValidGarden_SavesToService() throws Exception {
+        Garden garden = new Garden("gardenName", "gardenLocation", 1.0);
+        Mockito.when(gardenService.addGarden(Mockito.any())).thenReturn(garden);
 
-    @Test
-    public void CreateGardenPost_WithValidGarden_CreatesGardenInService() throws Exception {
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/gardens/create").param("gardenName", "gardenName")
                         .param("gardenLocation", "gardenLocation").param("gardenSize", "1"))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.redirectedUrl("/gardens/1")).andDo(result -> {
-                    Garden createdGarden = gardenService.getGardenById(1);
-                    assertEquals("gardenName", createdGarden.getName());
-                    assertEquals("gardenLocation", createdGarden.getLocation());
-                    assertEquals(1, createdGarden.getSize());
-                });
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+
+        Mockito.verify(gardenService, Mockito.times(1)).addGarden(Mockito.any());
     }
 
     @Test
-    public void EditGardenPost_WithValidGarden_CreatesGardenInService() throws Exception {
+    public void CreateGardenPost_WithInvlidGardenNameEmptyString_ReturnsError() throws Exception {
         Garden garden = new Garden("gardenName", "gardenLocation", 1.0);
-        gardenRepository.save(garden);
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/gardens/1/edit").param("gardenName", "gardenName")
-                        .param("gardenLocation", "gardenLocation").param("gardenSize", "2"))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.redirectedUrl("/gardens/1")).andDo(result -> {
-                    Garden createdGarden = gardenService.getGardenById(1);
-                    assertEquals("gardenName", createdGarden.getName());
-                    assertEquals("gardenLocation", createdGarden.getLocation());
-                    assertEquals(2.0, createdGarden.getSize());
-                });
+        Mockito.when(gardenService.addGarden(Mockito.any())).thenReturn(garden);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/gardens/create").param("gardenName", "")
+                .param("gardenLocation", "gardenLocation").param("gardenSize", "1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("gardenNameError"));
+
+        Mockito.verify(gardenService, Mockito.times(0)).addGarden(Mockito.any());
     }
 
     @Test
-    public void EditGardenPost_ClearSetSize_UpdatesGardenInServiceSizeToNull() throws Exception {
+    public void CreateGardenPost_WithInvalidGardenLocationEmptyString_ReturnsError()
+            throws Exception {
         Garden garden = new Garden("gardenName", "gardenLocation", 1.0);
-        gardenRepository.save(garden);
+        Mockito.when(gardenService.addGarden(Mockito.any())).thenReturn(garden);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/gardens/create").param("gardenName", "gardenName")
+                        .param("gardenLocation", "").param("gardenSize", "1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("gardenLocationError"));
+
+        Mockito.verify(gardenService, Mockito.times(0)).addGarden(Mockito.any());
+    }
+
+
+    @Test
+    public void EditGardenPost_InvalidNameEmptyString_ReturnsError() throws Exception {
+        Garden garden = new Garden("gardenName", "gardenLocation", 1.0);
+        Mockito.when(gardenService.getGardenById(1L)).thenReturn(garden);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/gardens/1/edit").param("gardenName", "")
+                .param("gardenLocation", "gardenLocation").param("gardenSize", "2"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("gardenNameError"));
+    }
+
+    @Test
+    public void EditGardenPost_InvalidLocationEmptyString_ReturnsError() throws Exception {
+        Garden garden = new Garden("gardenName", "gardenLocation", 1.0);
+        Mockito.when(gardenService.getGardenById(1L)).thenReturn(garden);
+
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/gardens/1/edit").param("gardenName", "gardenName")
-                        .param("gardenLocation", "gardenLocation").param("gardenSize", ""))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.redirectedUrl("/gardens/1")).andDo(result -> {
-                    Garden createdGarden = gardenService.getGardenById(1);
-                    assertNull(createdGarden.getSize());
-                });
+                        .param("gardenLocation", "").param("gardenSize", "2"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("gardenLocationError"));
     }
 }
