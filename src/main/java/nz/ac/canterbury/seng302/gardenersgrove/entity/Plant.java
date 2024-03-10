7c2d55b7ba99cbb91933f30f367634277c6800ed
Stephen Hockey (sho151) <sho151@uclive.ac.nz>
2024-03-10 14:55:50 +1300

    Made PlantCount an optional Integer type, made plantedOnDate a LocalDate, rather than a string, made sure previous tests still worked, wrote new tests.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/Plant.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/Plant.java
@@ -17,14 +17,14 @@ public class Plant {
 
     @Column(nullable = false)
     private String name;
-    @Column(nullable = false)
-    private int plantCount;
+    @Column
+    private Integer plantCount;
     @Column(nullable = false)
     private String description;
     @Column(nullable = false)
-    private String plantedOnDate;
+    private LocalDate plantedOnDate;
     @Column(nullable = false)
-    private int gardenId;
+    private Long gardenId;
 
     /**
      * JPA required no-args constructor
@@ -35,17 +35,35 @@ public class Plant {
      * Creates a new Plant object
      *
      * @param name name of plant
-     * @param count number of plant occurrence in garden
+     * @param plantCount number of plant occurrence in garden
      * @param description description of plant
      * @param plantedOnDate date when plant was planted (in DD/MM/YYYY format)
      * @param gardenId ID of garden where the plant is currently in
      */
-    public Plant(String name, String count, String description, String plantedOnDate, String gardenId) {
-        setName(name);
-        setCount(count);
-        setDescription(description);
-        setPlantedOnDate(plantedOnDate);
-        setGardenId(gardenId);
+    public Plant(String name, Integer plantCount, String description, LocalDate plantedOnDate, Long gardenId) {
+        this.name = name;
+        this.plantCount = plantCount;
+        this.description = description;
+        this.plantedOnDate = plantedOnDate;
+        this.gardenId = gardenId;
+    }
+
+
+    /**
+     * Creates a new Plant object given strings for each attribute
+     *
+     * @param name name of plant
+     * @param plantCount number of plant occurrence in garden
+     * @param description description of plant
+     * @param plantedOnDate date when plant was planted (in DD/MM/YYYY format)
+     * @param gardenId ID of garden where the plant is currently in
+     */
+    public Plant(String name, String plantCount, String description, String plantedOnDate, String gardenId) {
+        this.name = name;
+        this.setPlantCount(plantCount);
+        this.description = description;
+        this.setPlantedOnDate(plantedOnDate);
+        this.setGardenId(gardenId);
     }
 
     public Long getId() {
@@ -56,7 +74,7 @@ public class Plant {
         return name;
     }
 
-    public int getCount() {
+    public Integer getPlantCount() {
         return plantCount;
     }
 
@@ -64,28 +82,51 @@ public class Plant {
         return description;
     }
 
-    public String getPlantedOnDate() { return plantedOnDate; }
+    public LocalDate getPlantedOnDate() { return plantedOnDate; }
 
-    public int getGardenId() { return gardenId; }
+    public Long getGardenId() { return gardenId; }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setCount(String plantCount) { this.plantCount = Integer.parseInt(plantCount); }
+    public void setPlantCount(Integer plantCount) {
+        this.plantCount = plantCount;
+    }
+
+    /**
+     * Sets plantCount from a string representation
+     * @param plantCount the amount of this plant in this garden (optional)
+     */
+    public void setPlantCount(String plantCount) {
+        this.plantCount = (plantCount.isBlank()) ? null : Integer.parseInt(plantCount);
+    }
 
     public void setDescription(String description) { this.description = description; }
 
-    public void setPlantedOnDate(String plantedOnDate) {
-        String[] dateList = plantedOnDate.split("/");
-        int[] dateAsInteger = {Integer.parseInt(dateList[0]), Integer.parseInt(dateList[1]), Integer.parseInt(dateList[2])}; // Day, Month, Year
-        LocalDate newDate = LocalDate.of(dateAsInteger[2], dateAsInteger[1], dateAsInteger[0]);
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        this.plantedOnDate = newDate.format(dateTimeFormatter);
+    public void setPlantedOnDate(LocalDate plantedOnDate) {
+        this.plantedOnDate = plantedOnDate;
     }
 
-    public void setGardenId(String gardenid) {
-        this.gardenId = Integer.parseInt(gardenid);
+    /**
+     * Sets plantedOnDate given a string representation in DD/MM/YYYY format
+     * @param plantedOnDate string representing the date of planting
+     */
+    public void setPlantedOnDate(String plantedOnDate) {
+        String[] dateList = plantedOnDate.split("/");
+        this.plantedOnDate = LocalDate.of(Integer.parseInt(dateList[2]), Integer.parseInt(dateList[1]), Integer.parseInt(dateList[0]));
+    }
+
+    public void setGardenId(Long gardenId) {
+        this.gardenId = gardenId;
+    }
+
+    /**
+     * Sets gardenId from a String representation
+     * @param gardenId the id of the garden this plant belongs to
+     */
+    public void setGardenId(String gardenId) {
+        this.gardenId = Long.parseLong(gardenId);
     }
 
     @Override
