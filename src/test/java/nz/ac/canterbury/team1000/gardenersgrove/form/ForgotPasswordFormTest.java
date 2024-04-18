47d7ad84daf8d46bddd61497992e8a62060768c8
Elena Joeng <cjo108@uclive.ac.nz>
2024-04-18 17:33:38 +1200

    Added basic test outline

---
+++ src/test/java/nz/ac/canterbury/team1000/gardenersgrove/form/ForgotPasswordFormTest.java
@@ -0,0 +1,42 @@
+package nz.ac.canterbury.team1000.gardenersgrove.form;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.validation.BindingResult;
+public class ForgotPasswordFormTest {
+    ForgotPasswordForm forgotPasswordForm;
+
+    @Mock
+    BindingResult bindingResult;
+
+    @BeforeEach
+    void setUp() {
+        forgotPasswordForm.setEmail("john@doe.com");
+
+        bindingResult = Mockito.mock(BindingResult.class);
+        Mockito.when(bindingResult.hasErrors()).thenReturn(false);
+        Mockito.doAnswer(invocation -> {
+            Mockito.when(bindingResult.hasErrors()).thenReturn(true);
+            return null;
+        }).when(bindingResult).addError(Mockito.any());
+    }
+
+    @Test
+    void validate_WithBlankEmail_AddsError() {
+        forgotPasswordForm.setEmail("");
+        ForgotPasswordForm.validate(forgotPasswordForm, bindingResult);
+        Mockito.verify(bindingResult).addError(Mockito.any());
+    }
+
+    @Test
+    void validate_WithInvalidEmail_AddsError() {
+        forgotPasswordForm.setEmail("john@doe");
+        ForgotPasswordForm.validate(forgotPasswordForm, bindingResult);
+        Mockito.verify(bindingResult).addError(Mockito.any());
+    }
+
+    // Add tests later - check if provided email exists in the database
+    // If it does not exist, do we return error?
+}
