2344fb7d79cb303262a696ba4bdb3661615cd54f
Ahmick Montana <amo255@uclive.ac.nz>
2024-04-29 15:15:14 +1200

    Changed location API to LocationIQ for now, and refactored the classes so it uses LocationIQ. Still awaiting for spike approval so this won't be the final LocationIQ yet.

--- src/main/java/nz/ac/canterbury/team1000/gardenersgrove/api/LocationSearchService.java
+++ src/main/java/nz/ac/canterbury/team1000/gardenersgrove/api/LocationSearchService.java
@@ -1,179 +1,84 @@
 package nz.ac.canterbury.team1000.gardenersgrove.api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import nz.ac.canterbury.team1000.gardenersgrove.entity.Location;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class LocationSearchService {
-    private final RestTemplate restTemplate;
-    private final ObjectMapper objectMapper;
+    private final String API_KEY = "3d6f6041406e4a6eb3ab2c2aa117c9b6"; // Right now this is using my personal account, create a new account for Team 1000
+    private final String URL = "https://api.locationiq.com/v1/autocomplete";
 
-    public LocationSearchService() {
-        this.restTemplate = new RestTemplate();
-        this.objectMapper = new ObjectMapper();
-    }
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    /**
-     * This function searches for exact locations of the address entered by the user
-     * @param address the string address entered by user (e.g. Ilam Road)
-     * @param type the type of address field the address is (e.g. so it does not search for cities named Ilam Road, etc.)
-     * @return a list of geographic coordinates of locations that match the address + type
-     */
-    public List<List<String>> searchAddress(String address, String type) {
-        String url = String.format("https://nominatim.openstreetmap.org/search?q=%s&format=json", address);
+    public LocationSearchService() {}
+
+    public List<Location> searchLocations(String query, String type) {
+        // CONSTRUCT URL
+        String url = URL + "?q=" + query + "&limit=5&key=" + API_KEY;
+
+        // SEND GET REQUEST TO API ENDPOINT
         String jsonResponse = restTemplate.getForObject(url, String.class);
 
-        List<List<String>> coordinates = new ArrayList<>();
+        List<Location> locationAddresses = new ArrayList<>();
 
+        // PARSE THE JSON RESPONSE WITH A TRY-CATCH
+        // RETURN LOCATION LIST IF THERE ARE RESULTS, RETURN EMPTY LIST OTHERWISE
         try {
             List<Map<String, Object>> locations = objectMapper.readValue(jsonResponse, List.class);
-            Iterator<Map<String, Object>> iterator = locations.iterator();
 
-            while (iterator.hasNext()) {
-                Map<String, Object> location = iterator.next();
-                if (type.equals(location.get("addresstype"))) {
-                    List<String> newCoordinate = new ArrayList<>();
-                    newCoordinate.add(location.get("lat").toString());
-                    newCoordinate.add(location.get("lon").toString());
-                    coordinates.add(newCoordinate);
+            for (Map<String, Object> location : locations) {
+                Map<String, Object> address = (Map<String, Object>) location.get("addresss");
+                Location newLocation = new Location();
+                if (type.equals("city") && location.get("type").equals("city")) {
+                    newLocation.setCity(address.get("name").toString());
+                    newLocation.setCountry(address.get("country").toString());
+                } else if (type.equals("suburb") && location.get("type").equals("suburb")) {
+                    newLocation.setSuburb(address.get("name").toString());
+                    newLocation.setCity(address.get("city").toString());
+                    newLocation.setCountry(address.get("country").toString());
+                } else if (type.equals("street address")) {
+                    if (address.get("type").equals("road")) {
+                        newLocation.setStreet(address.get("name").toString());
+                        newLocation.setSuburb(address.get("suburb").toString());
+                        newLocation.setCity(address.get("city").toString());
+                        newLocation.setPostCode(address.get("postcode").toString());
+                        newLocation.setCountry(address.get("country").toString());
+                    } else {
+                        newLocation.setHouseNumber(address.get("house_number").toString());
+                        newLocation.setStreet(address.get("road").toString());
+                        newLocation.setSuburb(address.get("suburb").toString());
+                        newLocation.setCity(address.get("city").toString());
+                        newLocation.setPostCode(address.get("postcode").toString());
+                        newLocation.setCountry(address.get("country").toString());
+                    }
+                }
+                if (newLocation.getCity() != null && newLocation.getCountry() != null) {
+                    locationAddresses.add(newLocation);
                 }
             }
-            return coordinates;
+            return locationAddresses;
         } catch (Exception e) {
-            System.out.println("yo this a problem");
-            return null;
+            return new ArrayList<>();
         }
     }
 
-    /**
-     * This function searches for the exact location by given latitude and longitude coordinates
-     * @param coordinates the coordinates (latitude, longitude) used to locate the exact location
-     * @return a Location object that contains values of the exact location
-     */
-    public Location getLocationByCoordinates(List<String> coordinates) {
-        String latitude = coordinates.get(0);
-        String longitude = coordinates.get(1);
-
-        String url = String.format("https://nominatim.openstreetmap.org/reverse?lat=%s&lon=%s&format=json", latitude, longitude);
-        String jsonResponse = restTemplate.getForObject(url, String.class);
-
-        try {
-            Map locations = objectMapper.readValue(jsonResponse, Map.class);
-
-            Map<String, Object> address = (Map<String, Object>) locations.get("address");
-
-            Location location = new Location();
-            if (address.containsKey("house_number")) {
-                location.setHouseNumber((String) address.get("house_number"));
-            }
-
-            if (address.containsKey("road")) {
-                location.setStreet((String) address.get("road"));
-            }
-
-            if (address.containsKey("suburb")) {
-                location.setSuburb((String) address.get("suburb"));
-            }
-
-            if (address.containsKey("city")) {
-                location.setCity((String) address.get("city"));
-            } else {
-                return null;
-            }
-
-            if (address.containsKey("postcode")) {
-                location.setPostCode((String) address.get("postcode"));
-            }
-
-            if (address.containsKey("country")) {
-                location.setCountry((String) address.get("country"));
-            } else {
-                return null;
-            }
-
-            return location;
-
-        } catch (JsonMappingException e) {
-            System.out.println("yo this a problem");
-        } catch (JsonProcessingException e) {
-            System.out.println("yo this a problem");
-        }
-
-        return null;
-    }
-
-    /**
-     * This function obtains coordinates of multiple locations in a given list and uses the
-     * getLocationByCoordinates() function to find the exact locations of the coordinates
-     * @param coordinatesList list of coordinates of locations
-     * @return list of Location objects that hold values of the searched locations
-     */
-    public List<Location> getAllLocations(List<List<String>> coordinatesList) {
-        List<Location> allLocations = new ArrayList<>();
-
-        for (int i = 0; i < coordinatesList.size(); i++) {
-            Location location = getLocationByCoordinates(coordinatesList.get(i));
-
-            if (location != null) {
-                allLocations.add(location);
-            }
-        }
-        return allLocations;
-    }
-
-    // temporary main function to experiment
-    public static void main(String[] args) {
+    private static void main(String[] args) {
         LocationSearchService locationSearchService = new LocationSearchService();
 
-        /**
-         * Types for type:
-         * Street Number: house_number
-         * Street: road
-         * Suburb: suburb
-         * City: city
-         * Postcode: postcode
-         * Country: country
-         */
+        List<Location> location = locationSearchService.searchLocations("Christchurch", "city");
 
-        List<List<String>> addresses = locationSearchService.searchAddress("Christchurch", "city");
-
-        List<Location> allAddresses = locationSearchService.getAllLocations(addresses);
-
-        for (Location address : allAddresses) {
-
-            if (address.getHouseNumber() != null) {
-                System.out.println("House Number: " + address.getHouseNumber());
-            }
-
-            if (address.getStreet() != null) {
-                System.out.println("Street: " + address.getStreet());
-            }
-
-            if (address.getSuburb() != null) {
-                System.out.println("Suburb: " + address.getSuburb());
-            }
-
-            if (address.getCity() != null) {
-                System.out.println("City: " + address.getCity());
-            }
-
-            if (address.getPostcode() != null) {
-                System.out.println("Postcode: " + address.getPostcode());
-            }
-
-            if (address.getCountry() != null) {
-                System.out.println("Country: " + address.getCountry());
-            }
-
-            System.out.println();
-
+        for (Location loc : location) {
+            System.out.println(loc);
         }
     }
 }
