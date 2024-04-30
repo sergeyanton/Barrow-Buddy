6ad3cfc0149fa2a938ccb871166197c5e172227d
sho151 <sho151@uclive.ac.nz>
2024-04-30 16:27:01 +1200

    Added controller that maps /locationSearch to a Java function that returns a list of locations.

---
+++ src/main/java/nz/ac/canterbury/team1000/gardenersgrove/api/LocationSearchController.java
@@ -0,0 +1,23 @@
+package nz.ac.canterbury.team1000.gardenersgrove.api;
+
+import nz.ac.canterbury.team1000.gardenersgrove.entity.Location;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LocationSearchController {
+    private final LocationSearchService locationSearchService;
+
+    public LocationSearchController(LocationSearchService locationSearchService) {
+        this.locationSearchService = locationSearchService;
+    }
+
+    @GetMapping("/locationSearch")
+    public List<Location> searchLocations(@RequestParam(name = "query") String query, @RequestParam(name = "addressField") String addressField) {
+        return locationSearchService.searchLocations(query, addressField);
+    }
+}
