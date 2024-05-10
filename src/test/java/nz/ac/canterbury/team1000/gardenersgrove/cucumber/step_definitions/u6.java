784bd2bb4721d594fe1c299705e97418211f151a
Sergey Antonets <san177@uclive.ac.nz>
2024-05-10 16:44:07 +1200

    set up cucumber tests

---
+++ src/test/java/nz/ac/canterbury/team1000/gardenersgrove/cucumber/step_definitions/u6.java
@@ -0,0 +1,21 @@
+package nz.ac.canterbury.team1000.gardenersgrove.cucumber.step_definitions;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+public class u6 {
+@Given("I have registered with the first name {string} and last name {string}, email {string} and password {string}")
+public void i_have_registered_with_the_first_name_and_last_name_email_and_password(String string,String string2,String string3,String string4) {
+// Write code here that turns the phrase above into concrete actions
+
+}
+
+  @When("I don't verify my account and try log in")
+  public void iDonTVerifyMyAccountAndTryLogIn() {
+
+
+  }
+
+  @Then("I am redirected to the page with URL {string}")
+  public void iAmRedirectedToThePageWithURL(String arg0) {
+  }
+}
