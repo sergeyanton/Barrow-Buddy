659fca16e68fc56d4c678ea7a7bf0c67d1de4057
Sergey Antonets <san177@uclive.ac.nz>
2024-05-10 17:06:02 +1200

    started writing cucumber test for verification of an account

--- src/test/java/nz/ac/canterbury/team1000/gardenersgrove/cucumber/step_definitions/VerificationAfterRegister.java
+++ src/test/java/nz/ac/canterbury/team1000/gardenersgrove/cucumber/step_definitions/VerificationAfterRegister.java
@@ -1,13 +1,18 @@
 package nz.ac.canterbury.team1000.gardenersgrove.cucumber.step_definitions;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-public class u6 {
-@Given("I have registered with the first name {string} and last name {string}, email {string} and password {string}")
-public void i_have_registered_with_the_first_name_and_last_name_email_and_password(String string,String string2,String string3,String string4) {
-// Write code here that turns the phrase above into concrete actions
+public class VerificationAfterRegister {
+  @BeforeAll
+  public static void before_or_after_all() {
 
-}
+    
+  }
+  @Given("I have registered with the first name {string} and last name {string}, email {string} and password {string}")
+  public void iHaveRegisteredWithValidCredentials(String firstName, String lastName, String email, String password) {
+
+  }
 
   @When("I don't verify my account and try log in")
   public void iDonTVerifyMyAccountAndTryLogIn() {
