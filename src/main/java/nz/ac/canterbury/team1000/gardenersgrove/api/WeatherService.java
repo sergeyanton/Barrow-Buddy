efd3b2a9648043579daad3028bd07c87c9ed9ba7
sho151 <sho151@uclive.ac.nz>
2024-05-12 23:09:12 +1200

    Figured out how to parse the json, and made a quick enum to turn the weather codes (numbers) into useful weather descriptions.

--- src/main/java/nz/ac/canterbury/team1000/gardenersgrove/api/WeatherService.java
+++ src/main/java/nz/ac/canterbury/team1000/gardenersgrove/api/WeatherService.java
@@ -1,23 +1,35 @@
 package nz.ac.canterbury.team1000.gardenersgrove.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import nz.ac.canterbury.team1000.gardenersgrove.entity.WeatherData;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
-  private final String URL = "https://api.open-meteo.com/v1/forecast?latitude=-43.5333&longitude=172.6333&hourly=weather_code";
-  private RestTemplate restTemplate = new RestTemplate();
-  private final ObjectMapper objectMapper = new ObjectMapper();
+    private final String URL = "https://api.open-meteo.com/v1/forecast?latitude=-43.5333&longitude=172.6333&daily=weather_code&forecast_days=5";
+    private RestTemplate restTemplate = new RestTemplate();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-  public WeatherService() {}
+    public WeatherService() {}
 
-  public WeatherService(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
+    public WeatherService(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
 
-  public WeatherData getWeatherData() {
-    String jsonResponse = restTemplate.getForObject(URL, String.class);
-    WeatherData weatherData = new WeatherData(jsonResponse);
-    return weatherData;
-  }
+    public WeatherData getWeatherData() {
+        String jsonResponse = restTemplate.getForObject(URL, String.class);
+        try {
+            Map<String, Object> weather = objectMapper.readValue(jsonResponse, Map.class);
+            List<String> weatherCodes = (List<String>) ((Map<String, Object>) weather.get("daily")).get("weather_code");
+            System.out.println(weatherCodes);
+
+			return new WeatherData(jsonResponse);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 }
---
+++ src/main/java/nz/ac/canterbury/team1000/gardenersgrove/api/WeatherType.java
@@ -0,0 +1,39 @@
+package nz.ac.canterbury.team1000.gardenersgrove.api;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum WeatherType {
+	//TODO MORE BETTER ENUMS
+	NO_RAIN(new int[]{0, 1, 2, 3, 45, 48, 71, 73, 75, 77, 85, 86, 96, 99}, "It did not rain", ""),
+	RAIN(new int[]{51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82, 95}, "It rained", "");
+	private final int[] codes;
+	private final String text;
+	private final String imagePath;
+
+	WeatherType(int[] codes, String text, String imagePath) {
+		this.codes = codes;
+		this.text = text;
+		this.imagePath = imagePath;
+	}
+
+	public int[] getCodes() {
+		return codes;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public static WeatherType getByCode(int code) {
+		Optional<WeatherType> optionalType = Arrays.stream(WeatherType.values())
+			.filter(type -> Arrays.stream(type.codes).anyMatch(c -> c == code))
+			.findFirst();
+
+		return optionalType.orElse(null);
+	}
+}
