6182880bcc9281c114ce38adc74d02a2382e6732
Sergey Antonets <san177@uclive.ac.nz>
2024-05-15 17:21:24 +1200

    fix

--- src/test/java/nz/ac/canterbury/team1000/gardenersgrove/cucumber/step_definitions/VerificationAfterRegister.java
+++ src/test/java/nz/ac/canterbury/team1000/gardenersgrove/cucumber/step_definitions/VerificationAfterRegister.java
@@ -16,11 +16,9 @@ import nz.ac.canterbury.team1000.gardenersgrove.service.GardenService;
 import nz.ac.canterbury.team1000.gardenersgrove.service.ResetTokenService;
 import nz.ac.canterbury.team1000.gardenersgrove.service.UserService;
 import nz.ac.canterbury.team1000.gardenersgrove.service.VerificationTokenService;
-<<<<<<< Updated upstream
 import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Assertions;
-=======
->>>>>>> Stashed changes
+
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -35,47 +33,19 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-<<<<<<< Updated upstream
 public class VerificationAfterRegister {
+
   private final MockMvc mockMvc;
-=======
-@WebMvcTest(AccountController.class)
-@AutoConfigureMockMvc
-public class VerificationAfterRegister {
 
-  @Autowired
-  private MockMvc mockMvc;
-
-  @MockBean
-  private UserService userService;
-
-  @MockBean
-  private EmailService emailService;
-
-  @MockBean
-  private VerificationTokenService verificationTokenService;
-
-  @MockBean
-  private ResetTokenService resetTokenService;
-
-  @MockBean
-  private GardenService gardenService;
-
-  @MockBean
-  private AuthenticationManager authenticationManager;
-
-  @MockBean
-  private PasswordEncoder passwordEncoder;
->>>>>>> Stashed changes
 
   public VerificationAfterRegister(CucumberSpringConfiguration cucumberSpringConfiguration) {
     this.mockMvc = cucumberSpringConfiguration.getMockMvc();
     Assertions.assertNotNull(this.mockMvc);
   }
+
   @MockBean
   private UserService userService;
 
-<<<<<<< Updated upstream
   @MockBean
   private EmailService emailService;
 
@@ -87,7 +57,7 @@ public class VerificationAfterRegister {
 
   @MockBean
   private GardenService gardenService;
-=======
+
   @Mock
   private VerificationToken verificationTokenMock;
 
@@ -100,24 +70,16 @@ public class VerificationAfterRegister {
 
     Mockito.when(userService.checkEmail(Mockito.any())).thenReturn(false);
     Mockito.when(userService.isSignedIn()).thenReturn(false);
-    Mockito.when(passwordEncoder.matches(Mockito.any(), Mockito.any())).thenReturn(true);
->>>>>>> Stashed changes
-
-  @MockBean
-  private AuthenticationManager authenticationManager;
-
-  @MockBean
-  private PasswordEncoder passwordEncoder;
+//    Mockito.when(passwordEncoder.matches(Mockito.any(), Mockito.any())).thenReturn(true);
+  }
 
   @Given("I have registered with the first name {string} and last name {string}, email {string} and password {string}")
-  public void iHaveRegisteredWithValidCredentials(String firstName, String lastName, String email,
+  public void iHaveRegisteredWithValidCredentials(String firstName, String lastName, String
+      email,
       String password) throws Exception {
-<<<<<<< Updated upstream
     RegistrationForm registrationForm = new RegistrationForm();
-=======
 
     User newUser = new User(firstName, lastName, email, password, LocalDate.now(), " ");
->>>>>>> Stashed changes
     registrationForm.setFirstName(firstName);
     registrationForm.setLastName(lastName);
     registrationForm.setEmail(email);
@@ -126,7 +88,6 @@ public class VerificationAfterRegister {
     registrationForm.setDob("01/01/2000");
     registrationForm.setNoSurnameCheckBox(false);
 
-<<<<<<< Updated upstream
     MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/register").with(csrf())
         .flashAttr("registrationForm", registrationForm)).andReturn();
   }
@@ -172,24 +133,6 @@ public class VerificationAfterRegister {
 //    MOCK_MVC.perform(MockMvcRequestBuilders.get(pageURL))
 //        .andExpect(MockMvcResultMatchers.redirectedUrl(pageURL));
 //  }
-=======
-    Mockito.when(registrationForm.getUser(Mockito.any())).thenReturn(newUser);
-    mockMvc.perform(MockMvcRequestBuilders.post("/register").with(csrf())
-            .flashAttr("registrationForm", registrationForm))
-        .andExpect(MockMvcResultMatchers.redirectedUrl("/register/verification"));
-  }
-
-  @When("I access log in page without verifying my account")
-  public void iAccessLoginPageWithoutVerifyingMyAccount() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.get("/login"))
-        .andExpect(MockMvcResultMatchers.status().isOk());
-  }
-
-  @Then("I am redirected to the page with URL {string}")
-  public void iAmRedirectedToThePageWithURL(String pageURL) throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.get(pageURL))
-        .andExpect(MockMvcResultMatchers.redirectedUrl(pageURL));
-  }
->>>>>>> Stashed changes
 
 }
+
