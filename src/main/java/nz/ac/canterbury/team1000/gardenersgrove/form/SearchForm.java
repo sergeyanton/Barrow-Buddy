ea47ac483bfe8eac3ebac6c64c4f84537a3ebaf4
Elena Joeng <cjo108@uclive.ac.nz>
2024-05-15 17:24:33 +1200

    Created Search Form to validate the email

---
+++ src/main/java/nz/ac/canterbury/team1000/gardenersgrove/form/SearchForm.java
@@ -0,0 +1,29 @@
+package nz.ac.canterbury.team1000.gardenersgrove.form;
+
+import static nz.ac.canterbury.team1000.gardenersgrove.form.FormUtils.MAX_DB_STR_LEN;
+import static nz.ac.canterbury.team1000.gardenersgrove.form.FormUtils.checkBlank;
+import static nz.ac.canterbury.team1000.gardenersgrove.form.FormUtils.checkEmailIsInvalid;
+import static nz.ac.canterbury.team1000.gardenersgrove.form.FormUtils.checkOverMaxLength;
+
+import org.springframework.validation.BindingResult;
+
+public class SearchForm {
+	protected String email;
+	public String getEmail() {
+		return email;
+	}
+
+	public static void validate(SearchForm searchForm, BindingResult bindingResult) {
+		ErrorAdder errors = new ErrorAdder(bindingResult, "searchForm");
+
+		// validate email
+		if (checkBlank(searchForm.getEmail()) || checkEmailIsInvalid(searchForm.getEmail())) {
+			errors.add("email", "Email address must be in the form ‘jane@doe.nz’",
+				searchForm.getEmail());
+		} else if (checkOverMaxLength(searchForm.getEmail(), MAX_DB_STR_LEN)) {
+			errors.add("email",
+				"Email address must be " + MAX_DB_STR_LEN + " characters long or less",
+				searchForm.getEmail());
+		}
+	}
+}
