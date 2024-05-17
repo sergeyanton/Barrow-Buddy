b34fd8137e244e8a30d25c546e704e052d39f99b
Hanan Fokkens <hfo22@uclive.ac.nz>
2024-05-17 16:04:02 +1200

    Working cucumber tests, needs mocks for Email and other API services

--- src/test/java/nz/ac/canterbury/team1000/gardenersgrove/cucumber/RunCucumberTest.java
+++ src/test/java/nz/ac/canterbury/team1000/gardenersgrove/cucumber/RunCucumberTest.java
@@ -1,24 +1,17 @@
 package nz.ac.canterbury.team1000.gardenersgrove.cucumber;
 
-import io.cucumber.java.Scenario;
 import io.cucumber.junit.platform.engine.Constants;
 import io.cucumber.spring.CucumberContextConfiguration;
 import nz.ac.canterbury.team1000.gardenersgrove.GardenersGroveApplication;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.platform.suite.api.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 /**
  * This a required class to set up the cucumber tests. This code was taken from ATDD workshop
  */
-@AutoConfigureMockMvc
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
@@ -27,9 +20,11 @@ import org.springframework.web.context.WebApplicationContext;
     @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report/cucumber.html"),
     @ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
 })
-@ContextConfiguration(classes = {GardenersGroveApplication.class})
+@ContextConfiguration(classes = GardenersGroveApplication.class)
+@CucumberContextConfiguration
 @SpringBootTest
 @ActiveProfiles("cucumber")
+@AutoConfigureMockMvc
 public class RunCucumberTest {
 }
 
--- src/test/java/nz/ac/canterbury/team1000/gardenersgrove/cucumber/step_definitions/VerificationAfterRegister.java
+++ src/test/java/nz/ac/canterbury/team1000/gardenersgrove/cucumber/step_definitions/VerificationAfterRegister.java
@@ -2,90 +2,39 @@ package nz.ac.canterbury.team1000.gardenersgrove.cucumber.step_definitions;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import java.time.LocalDate;
-import nz.ac.canterbury.team1000.gardenersgrove.controller.AccountController;
-import nz.ac.canterbury.team1000.gardenersgrove.cucumber.CucumberSpringConfiguration;
 import nz.ac.canterbury.team1000.gardenersgrove.entity.User;
-import nz.ac.canterbury.team1000.gardenersgrove.entity.VerificationToken;
 import nz.ac.canterbury.team1000.gardenersgrove.form.RegistrationForm;
 import nz.ac.canterbury.team1000.gardenersgrove.service.EmailService;
-import nz.ac.canterbury.team1000.gardenersgrove.service.GardenService;
-import nz.ac.canterbury.team1000.gardenersgrove.service.ResetTokenService;
 import nz.ac.canterbury.team1000.gardenersgrove.service.UserService;
-import nz.ac.canterbury.team1000.gardenersgrove.service.VerificationTokenService;
-import org.checkerframework.checker.units.qual.A;
-import org.junit.jupiter.api.Assertions;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 public class VerificationAfterRegister {
 
   @Autowired
-  private WebApplicationContext webApplicationContext;
   private MockMvc mockMvc;
 
 
-//  public VerificationAfterRegister(CucumberSpringConfiguration cucumberSpringConfiguration) {
-//    this.mockMvc = cucumberSpringConfiguration.getMockMvc();
-//    Assertions.assertNotNull(this.mockMvc);
-//  }
+  // public VerificationAfterRegister(CucumberSpringConfiguration cucumberSpringConfiguration) {
+  // this.mockMvc = cucumberSpringConfiguration.getMockMvc();
+  // Assertions.assertNotNull(this.mockMvc);
+  // }
 
-  @MockBean
+  @Autowired
   private UserService userService;
 
+  @Autowired
   @MockBean
-  private EmailService emailService;
-
-  @MockBean
-  private VerificationTokenService verificationTokenService;
-
-  @MockBean
-  private ResetTokenService resetTokenService;
-
-  @MockBean
-  private GardenService gardenService;
-
-  @MockBean
-  private AuthenticationManager authenticationManager;
-  @Mock
-  private VerificationToken verificationTokenMock;
-
-  private RegistrationForm registrationForm;
-
-  @Before
-  public void setUp() {
-    mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-  }
-  @BeforeEach
-  public void beforeEach() {
-    registrationForm = new RegistrationForm();
-
-    Mockito.when(userService.checkEmail(Mockito.any())).thenReturn(false);
-    Mockito.when(userService.isSignedIn()).thenReturn(false);
-//    Mockito.when(passwordEncoder.matches(Mockito.any(), Mockito.any())).thenReturn(true);
-  }
+  private EmailService emailService = Mockito.mock(EmailService.class);
 
   @Given("I have registered with the first name {string} and last name {string}, email {string} and password {string}")
-  public void iHaveRegisteredWithValidCredentials(String firstName, String lastName, String
-      email,
+  public void iHaveRegisteredWithValidCredentials(String firstName, String lastName, String email,
       String password) throws Exception {
     RegistrationForm registrationForm = new RegistrationForm();
 
@@ -102,47 +51,50 @@ public class VerificationAfterRegister {
         .flashAttr("registrationForm", registrationForm)).andReturn();
   }
 
-//  @Before
-//  public void beforeEach() {
-//    userService = new UserService(userRepository);
-//    verificationTokenService = new VerificationTokenService(verificationTokenRepository);
-//    resetTokenService = new ResetTokenService(resetTokenRepository);
-//    emailService = new EmailService(Mockito.mock(JavaMailSender.class));
-//    AccountController accountController = new AccountController(userService, verificationTokenService, emailService, resetTokenService);
-//
-//    MOCK_MVC = MockMvcBuilders.standaloneSetup(accountController).build();
-//    registrationForm = new RegistrationForm();
-//
-//
-//
-//  }
-//
-//  @Given("I have registered with the first name {string} and last name {string}, email {string} and password {string}")
-//  public void iHaveRegisteredWithValidCredentials(String firstName, String lastName, String email,
-//      String password) throws Exception {
-//    registrationForm.setFirstName(firstName);
-//    registrationForm.setLastName(lastName);
-//    registrationForm.setEmail(email);
-//    registrationForm.setPassword(password);
-//    registrationForm.setRetypePassword(password);
-//    registrationForm.setDob("01/01/2000");
-//    registrationForm.setNoSurnameCheckBox(false);
-//
-//    MvcResult result = MOCK_MVC.perform(MockMvcRequestBuilders.post("/register").with(csrf())
-//        .flashAttr("registrationForm", registrationForm)).andReturn();
-//  }
-//
-//  @When("I access log in page without verifying my account")
-//  public void iAccessLoginPageWithoutVerifyingMyAccount() throws Exception {
-//    MOCK_MVC.perform(MockMvcRequestBuilders.get("/login"))
-//        .andExpect(MockMvcResultMatchers.status().isOk());
-//  }
-//
-//  @Then("I am redirected to the page with URL {string}")
-//  public void iAmRedirectedToThePageWithURL(String pageURL) throws Exception {
-//    MOCK_MVC.perform(MockMvcRequestBuilders.get(pageURL))
-//        .andExpect(MockMvcResultMatchers.redirectedUrl(pageURL));
-//  }
+  // @Before
+  // public void beforeEach() {
+  // userService = new UserService(userRepository);
+  // verificationTokenService = new VerificationTokenService(verificationTokenRepository);
+  // resetTokenService = new ResetTokenService(resetTokenRepository);
+  // emailService = new EmailService(Mockito.mock(JavaMailSender.class));
+  // AccountController accountController = new AccountController(userService,
+  // verificationTokenService, emailService, resetTokenService);
+  //
+  // MOCK_MVC = MockMvcBuilders.standaloneSetup(accountController).build();
+  // registrationForm = new RegistrationForm();
+  //
+  //
+  //
+  // }
+  //
+  // @Given("I have registered with the first name {string} and last name {string}, email {string}
+  // and password {string}")
+  // public void iHaveRegisteredWithValidCredentials(String firstName, String lastName, String
+  // email,
+  // String password) throws Exception {
+  // registrationForm.setFirstName(firstName);
+  // registrationForm.setLastName(lastName);
+  // registrationForm.setEmail(email);
+  // registrationForm.setPassword(password);
+  // registrationForm.setRetypePassword(password);
+  // registrationForm.setDob("01/01/2000");
+  // registrationForm.setNoSurnameCheckBox(false);
+  //
+  // MvcResult result = MOCK_MVC.perform(MockMvcRequestBuilders.post("/register").with(csrf())
+  // .flashAttr("registrationForm", registrationForm)).andReturn();
+  // }
+  //
+  // @When("I access log in page without verifying my account")
+  // public void iAccessLoginPageWithoutVerifyingMyAccount() throws Exception {
+  // MOCK_MVC.perform(MockMvcRequestBuilders.get("/login"))
+  // .andExpect(MockMvcResultMatchers.status().isOk());
+  // }
+  //
+  // @Then("I am redirected to the page with URL {string}")
+  // public void iAmRedirectedToThePageWithURL(String pageURL) throws Exception {
+  // MOCK_MVC.perform(MockMvcRequestBuilders.get(pageURL))
+  // .andExpect(MockMvcResultMatchers.redirectedUrl(pageURL));
+  // }
 
 }
 
