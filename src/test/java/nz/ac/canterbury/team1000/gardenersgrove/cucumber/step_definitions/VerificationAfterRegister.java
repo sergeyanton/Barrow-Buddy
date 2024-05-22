2eba96b4e45ac44e3e2e151c9b08f6ebbe3f369e
Sergey Antonets <san177@uclive.ac.nz>
2024-05-23 11:38:08 +1200

    Fixed cucumber test to work with email service, cucumber test passes now

--- src/test/java/nz/ac/canterbury/team1000/gardenersgrove/cucumber/step_definitions/VerificationAfterRegister.java
+++ src/test/java/nz/ac/canterbury/team1000/gardenersgrove/cucumber/step_definitions/VerificationAfterRegister.java
@@ -7,6 +7,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import java.time.LocalDate;
 import nz.ac.canterbury.team1000.gardenersgrove.entity.User;
+import nz.ac.canterbury.team1000.gardenersgrove.form.LoginForm;
 import nz.ac.canterbury.team1000.gardenersgrove.form.RegistrationForm;
 import nz.ac.canterbury.team1000.gardenersgrove.service.EmailService;
 import nz.ac.canterbury.team1000.gardenersgrove.service.UserService;
@@ -40,12 +41,15 @@ public class VerificationAfterRegister {
   @Autowired
   private JavaMailSender emailSender;
 
+  private User testUser;
+
   @Given("I have registered with the first name {string} and last name {string}, email {string} and password {string}")
   public void iHaveRegisteredWithValidCredentials(String firstName, String lastName, String email,
       String password) throws Exception {
+
     RegistrationForm registrationForm = new RegistrationForm();
 
-    User newUser = new User(firstName, lastName, email, password, LocalDate.now(), " ");
+    testUser = new User(firstName, lastName, email, password, LocalDate.now(), " ");
     registrationForm.setFirstName(firstName);
     registrationForm.setLastName(lastName);
     registrationForm.setEmail(email);
@@ -54,19 +58,28 @@ public class VerificationAfterRegister {
     registrationForm.setDob("01/01/2000");
     registrationForm.setNoSurnameCheckBox(false);
 
-    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/register").with(csrf())
-        .flashAttr("registrationForm", registrationForm)).andReturn();
+    mockMvc.perform(MockMvcRequestBuilders.post("/register").with(csrf())
+        .flashAttr("registrationForm", registrationForm))
+        .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+        .andExpect(MockMvcResultMatchers.redirectedUrl("/register/verification"));
   }
 
-   @When("I access log in page without verifying my account")
-   public void iAccessLoginPageWithoutVerifyingMyAccount() throws Exception {
-     mockMvc.perform(MockMvcRequestBuilders.get("/login"))
-         .andExpect(MockMvcResultMatchers.status().isOk());
+   @When("I access {string} page without verifying my account")
+   public void iAccessLoginPageWithoutVerifyingMyAccount(String pageURL) throws Exception {
+     mockMvc.perform(MockMvcRequestBuilders.get(pageURL))
+         .andExpect(MockMvcResultMatchers.status().isOk())
+         .andExpect(MockMvcResultMatchers.view().name("pages/loginPage"));
   }
    @Then("I am redirected to the page with URL {string}")
    public void iAmRedirectedToThePageWithURL(String pageURL) throws Exception {
-      mockMvc.perform(MockMvcRequestBuilders.get(pageURL))
-     .andExpect(MockMvcResultMatchers.redirectedUrl(pageURL));
+      LoginForm loginForm = new LoginForm();
+      loginForm.setEmail(testUser.getEmail());
+      loginForm.setPassword(testUser.getPassword());
+
+      mockMvc.perform(MockMvcRequestBuilders.post("/login").with(csrf())
+          .flashAttr("loginForm", loginForm))
+          .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+          .andExpect(MockMvcResultMatchers.redirectedUrl(pageURL));
    }
 
 }
